b4b283e0285037cc9d947feb994095459f5d79cb
drew-tom1 <tom.drew261@gmail.com>
2025-03-01 21:33:21 -0800

    began work on frontend + dashboard, need to implement auth first for backend and component loading

---
+++ frontend/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import { Card } from "@/components/ui/card"
+
+export default function Page() {
+    return (
+        <main>
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+        </main>
+    )
+}
---
+++ frontend/components/header.tsx
@@ -0,0 +1,31 @@
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuIndicator,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+    NavigationMenuViewport,
+} from "@/components/ui/navigation-menu"
+
+export default function Header() {
+    return (
+        <NavigationMenu>
+            <NavigationMenuList>
+                <NavigationMenuItem>
+                <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+                <NavigationMenuContent>
+                    <NavigationMenuLink>Link</NavigationMenuLink>
+                </NavigationMenuContent>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+                <NavigationMenuContent>
+                    <NavigationMenuLink>Link</NavigationMenuLink>
+                </NavigationMenuContent>
+                </NavigationMenuItem>
+            </NavigationMenuList>
+            </NavigationMenu>
+    )
+}
