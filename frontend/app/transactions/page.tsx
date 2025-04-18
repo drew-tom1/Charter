c51945936504f122e409db88cba3cb03d05b7791
Drew Tom <tom.drew261@gmail.com>
2025-04-18 00:17:09 -0700

    starting transaction ledger

--- frontend/app/transactions/page.tsx
+++ frontend/app/transactions/page.tsx
@@ -1,6 +1,4 @@
 import { AppSidebar } from "@/components/app-sidebar"
-import { DataTable } from "@/components/data-table"
-import { SectionCards } from "@/components/section-cards"
 import { SiteHeader } from "@/components/site-header"
 import {
   SidebarInset,
@@ -8,6 +6,7 @@ import {
 } from "@/components/ui/sidebar"
 
 import data from './data.json'
+import { TransactionLedger } from "@/components/transaction-table"
 
 export default function Page() {
   return (
@@ -25,7 +24,7 @@ export default function Page() {
         <div className="flex flex-1 flex-col">
           <div className="@container/main flex flex-1 flex-col gap-2">
             <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-              <DataTable data={data}/>
+              <TransactionLedger data={data}/>
             </div>
           </div>
         </div>
